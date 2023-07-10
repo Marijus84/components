2ceaac9b3019b431b280a92b325e65da98a0a95d
Marijus84 <marijus.romanovas@hotmail.com>
2023-07-10 19:39:37 +0300

    add conditional rendering and looping

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,46 @@
 import "./Card.css";
+import Star from "../../assets/star.svg";
 
-const Card = (props) => {
+const Card = ({ title, description, rating }) => {
   const imageUrl =
     "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
+  //! early return
+  if (rating > 9) return null;
+
+  //! condtional rendering using if before return
+  if (rating > 8) {
+    return (
+      <div className="card">
+        <img src={imageUrl} alt="example_image" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {rating > 8 ? <h3>Rating: {rating}</h3> : <h6>Rating: {rating}</h6>}
+        {rating > 8 && (
+          <img className="star" src={Star} alt="high rating star" />
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="card">
-      <img src={imageUrl} alt="example_image" />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
+      <h3>Book is not worth reading</h3>
     </div>
   );
+
+  // return (
+  //   <div className="card">
+  //     <img src={imageUrl} alt="example_image" />
+  //     <h2>{title}</h2>
+  //     <p>{description}</p>
+  //! conditional rendering with ternary operator
+  //     {rating > 8 ? <h3>Rating: {rating}</h3> : <h6>Rating: {rating}</h6>}
+
+  //! conditional rendering with double && operator
+  //     {rating > 8 && <img className="star" src={Star} alt="high rating star" />}
+  //   </div>
+  // );
 };
 
 export default Card;
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,12 +1,54 @@
-import CardClass from "../card-class/CardClass";
 import Card from "../card/Card";
 
 const Content = () => {
+  const cardsData = [
+    {
+      id: 1,
+      title: "Harry Potter",
+      description: "Harry Potter is a fictional book written by J.K. Rowling",
+      rating: 8.5,
+    },
+    {
+      id: 2,
+      title: "1984",
+      description: "1984 George Orwell book",
+      rating: 9.2,
+    },
+    {
+      id: 3,
+      title: "The Lord of the Rings",
+      description:
+        "The Lord of the Rings is a fictional book written by J.R.R. Tolkien",
+      rating: 7.9,
+    },
+  ];
+
   return (
     <>
-      <Card title="Card One" description="I am first description" />
-      <Card title="Card Two" description="I am second description" />
-      <CardClass />
+      {cardsData.map(({ title, description, rating, id }) => (
+        <Card
+          key={id}
+          title={title}
+          description={description}
+          rating={rating}
+        />
+      ))}
+
+      {/* <Card
+        title={cardsData[0].title}
+        description={cardsData[0].description}
+        rating={cardsData[0].rating}
+      />
+      <Card
+        title={cardsData[1].title}
+        description={cardsData[1].description}
+        rating={cardsData[1].rating}
+      />
+      <Card
+        title={cardsData[2].title}
+        description={cardsData[2].description}
+        rating={cardsData[2].rating}
+      /> */}
     </>
   );
 };
